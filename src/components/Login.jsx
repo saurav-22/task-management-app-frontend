51d60d6a4302f79048b2ffc2c2afcbb5b75ca1a2
Saurav Singh <97401461+saurav-22@users.noreply.github.com>
2025-09-22 18:16:59 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,7 +12,7 @@ function Login({ setToken }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('//k8s-taskapp-taskappi-929be65fa1-1617985190.ap-south-1.elb.amazonaws.com/api/users/login', {
+      const response = await axios.post('https://de0e47pv5ccby.cloudfront.net/api/users/login', {
         email,
         password,
       });
@@ -61,4 +61,5 @@ function Login({ setToken }) {
 
 
 export default Login;
+
 
